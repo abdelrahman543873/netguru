d11a57f9d431ae4b9062baf56436c3355d4414e8
abdelrahman mousa <abdelrahmanmysara33@gmail.com>
2022-02-08 00:58:25 +0200

    refactor: added env refactoring

--- movies/src/main.ts
+++ movies/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { MoviesModule } from "./movies.module";
 import { MicroserviceOptions, Transport } from "@nestjs/microservices";
-import { BaseHttpExceptionFilter } from "../shared/exception-filter/base-http-exception-filter";
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     MoviesModule,
@@ -9,11 +8,12 @@ async function bootstrap() {
       transport: Transport.TCP,
       options: {
         port: 2000,
-        host: process.env.DOCKER_MOVIES_HOST || process.env.MOVIES_HOST,
+        ...(process.env.DOCKER_MOVIES_HOST && {
+          host: process.env.DOCKER_MOVIES_HOST,
+        }),
       },
     }
   );
-  app.useGlobalFilters(new BaseHttpExceptionFilter());
   app.listen();
 }
 bootstrap();
