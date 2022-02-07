079d543441470843f098babcde2d27e3a02d4bb8
abdelrahman mousa <abdelrahmanmysara33@gmail.com>
2022-02-08 01:22:53 +0200

    hotfix

--- movies/src/movies.controller.ts
+++ movies/src/movies.controller.ts
@@ -1,13 +1,13 @@
 import { MoviesService } from "./movies.service";
 import { Controller } from "@nestjs/common";
-import { MessagePattern } from "@nestjs/microservices";
+import { MessagePattern, Transport } from "@nestjs/microservices";
 import { AddMovieInput } from "./inputs/add-movie.input";
 
 @Controller("MOVIES")
 export class MoviesController {
   constructor(private moviesService: MoviesService) {}
 
-  @MessagePattern("add-movie")
+  @MessagePattern("add-movie", Transport.TCP)
   async addMovie(input: AddMovieInput) {
     return await this.moviesService.addMovie(input);
   }
