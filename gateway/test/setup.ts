c1707a2c0346eb845ac06585adcad0236f48d0e5
abdelrahman mousa <abdelrahmanmysara33@gmail.com>
2022-02-08 22:15:23 +0200

    feat: filtering test env

--- gateway/test/setup.ts
+++ gateway/test/setup.ts
@@ -1,20 +1,8 @@
-import fs from "fs";
-import { MongoMemoryServer } from "mongodb-memory-server";
 import { Test } from "@nestjs/testing";
 import { AppModule } from "../src/app.module";
 import { ValidationPipe } from "@nestjs/common";
 
-const mongod = new MongoMemoryServer({ binary: { version: "4.2.8" } });
 module.exports = async (): Promise<void> => {
-  await mongod.start();
-  const mongoConfig = {
-    mongoDBName: "jest",
-    mongoUri: await mongod.getUri(),
-  };
-  fs.writeFileSync("globalConfig.json", JSON.stringify(mongoConfig));
-  global.__MONGOD__ = mongod;
-  global.mongoUri = mongoConfig.mongoUri;
-  global.mongoDBName = mongoConfig.mongoDBName;
   const module = await Test.createTestingModule({
     imports: [AppModule],
   }).compile();
